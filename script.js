fc07e83cfef9d015356476e81ab4af04b6de234a
Sam Thabet <sallam.thabet@gmail.com>
2021-07-20 22:52:14 -0400

    Added a time indicator to header

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ body = document.body
 //Header Date
 src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"
 let date = moment().format('ll');
+let time = moment().format('LT');
 document.getElementById("currentDay").textContent = 'Today is ' + date;
-
+document.getElementById("currentTime").textContent = 'It is ' + time;
